002afc4d405962c11b8c1b813e16e2106e28176b
Ctrx Corporate <148440831+ctrxm@users.noreply.github.com>
2025-06-27 08:27:29 +0700

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,29 +1,29 @@
 <?php
 
-define('CLOUDFLARE_API_TOKEN', '8Yq9Yd2kW8wwZGA5w93XUHbJZk-ul9xsCn-hLjFS');
-define('CLOUDFLARE_EMAIL', 'hendra1rangga@gmail.com');
+define('CLOUDFLARE_API_TOKEN', 'YOUR_CF_API_TOKEN'); //Ganti Ini
+define('CLOUDFLARE_EMAIL', 'CF_EMAIL'); // Ganti Ini
 
-define('LICENSE_SERVER_API_URL', 'https://api.ctrxl.id/verify.php');
+define('LICENSE_SERVER_API_URL', 'https://api.ctrxl.id/verify.php'); //JANGAN DI HAPUS
 // --- Kredensial Database ---
 define('DB_HOST', 'localhost'); // Ganti Dengan Host Databasemu
-define('DB_USER', 'hendra'); // Ganti Dengan Username Databasemu
-define('DB_PASS', 'hendra'); // Ganti Dengan Password Databasemu
-define('DB_NAME', 'hendra'); // Ganti Dengan Nama Databasemu
+define('DB_USER', 'Ganti Ini'); // Ganti Dengan Username Databasemu
+define('DB_PASS', 'Ganti Ini'); // Ganti Dengan Password Databasemu
+define('DB_NAME', 'Ganti Ini'); // Ganti Dengan Nama Databasemu
 
 define('APP_SESSION_PREFIX', 'app_server_');
 
 // —- BOT TELEGRAM —-
 // --- Ubah TELEGRAM_BOT_TOKEN Dan TELGGRAM_CHAT_ID Punya Milikmu ---
-define('TELEGRAM_BOT_TOKEN', '7948821804:AAG9q_z4h5yeFIMvT29eTduJYDnBCd8PX7U');
-define('TELEGRAM_CHAT_ID', '-1002217281672');
+define('TELEGRAM_BOT_TOKEN', 'TG_BOT_TOKEN');
+define('TELEGRAM_CHAT_ID', 'TG_CHAT_ID_CHANNEL');
 
 // --- Kredensial Admin ---
 // --- Ubah ADMIN_USERNAME Dan ADMIN_PASSWORD_HASH ---
-define('ADMIN_USERNAME', 'hendra');
-define('ADMIN_PASSWORD_HASH', '$2a$12$klMJ0jcK6p9XkNcyIuYbPud6iq9OhfhmKEn6fYfCDWianHcEZmfaq');
+define('ADMIN_USERNAME', 'gantiini');
+define('ADMIN_PASSWORD_HASH', '$2a$12$klMJ0jcK6p9XkNcyIuYbPud6iq9OhfhmKEn6fYfCDWianHcEZmfaq'); // MD5 (Hendra123)
 
 
 // Untuk reCAPTCHA v3
 // --- Ubah RECAPTCHA_SITE_KEY Dan RECAPTCHA_SCREET_KEY ---
-define('RECAPTCHA_SITE_KEY', '6Lf2K2krAAAAAHA_IuBKlE7GLSoTKV-sBmzGXR9Z');
-define('RECAPTCHA_SECRET_KEY', '6Lf2K2krAAAAAJgepKK3z_8eQwHOwQLT1q-UzuNI');
+define('RECAPTCHA_SITE_KEY', 'RCPT_SITE_KEY');
+define('RECAPTCHA_SECRET_KEY', 'RCPT_SCRT_KEY');
